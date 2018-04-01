e4ed84184eb2cd34af00331bbfeaaee55a34ca16
RafM <rkjmosiolek@gmail.com>
2018-04-01 23:29:05 +0100

    add html structure and some basic UI styling

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,19 @@
-alert('gulp watch');
+const initMap = () => {
+    const mapContainer = document.querySelector("#map");
+    let mapOptions = {
+        center: {
+            lat: 51.5074,
+            lng: -0.1278
+        },
+        zoom: 13,
+        mapTypeId: 'terrain',
+    }
+    const map = new google.maps.Map(mapContainer, mapOptions);
+    const directionsService = new google.maps.DirectionsService;
+    const directionsDisplay = new google.maps.DirectionsRenderer({suppressMarkers: true, preserveViewport: false});
+    const geocoder = new google.maps.Geocoder;
+}
+
+const geolocateUser = () => {
+    
+}
--- build/js/main.js
+++ build/js/main.js
@@ -1,3 +1,19 @@
-'use strict';
+"use strict";
 
-alert('gulp watch');
+var initMap = function initMap() {
+    var mapContainer = document.querySelector("#map");
+    var mapOptions = {
+        center: {
+            lat: 51.5074,
+            lng: -0.1278
+        },
+        zoom: 13,
+        mapTypeId: 'terrain'
+    };
+    var map = new google.maps.Map(mapContainer, mapOptions);
+    var directionsService = new google.maps.DirectionsService();
+    var directionsDisplay = new google.maps.DirectionsRenderer({ suppressMarkers: true, preserveViewport: false });
+    var geocoder = new google.maps.Geocoder();
+};
+
+var geolocateUser = function geolocateUser() {};
